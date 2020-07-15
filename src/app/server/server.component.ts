df9e42a0656f2d1f2fc8a1006603aa75e65ddf6a
pavana kartik <pavanakartikvelagaelti@gmail.com>
2020-07-16 04:04:35 +0530

    stringinterpolationPropertyandEventBindingExMPLES

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
-  constructor() { }
+    serverId =10;
+    serverName ="App Server";
+    createServer= false;
+    newServerName= "my first server"
+   
+  constructor() {
+    
+    setTimeout(
+      ()=>{ this.createServer= true}, 2000
+    )
+  }
 
   ngOnInit(): void {
   }
+
+// PROPERTY BINDING EXAMPLES
+  allowNewServer(){
+    this.newServerName= "This is a brand new server";
+  }
+
+  // EVENT BINDING EXAMPLE
+ changeServerName(event: Event){
+   this.serverName= (<HTMLInputElement> event.target).value;
+ }
 
 }
